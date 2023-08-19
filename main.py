82da9f1d5b16bb044f6468a22a27e424c55f378f
Vansh Sachdeva <vansh2101sachdeva@gmail.com>
2023-08-19 11:13:42 +0530

    completed 3rd and 4th task

--- main.py
+++ main.py
@@ -54,13 +54,15 @@ cv2.destroyAllWindows()
 
 
 #* Counting the number of huts
-def count_triangles(img):
+def count_triangles(img, red, blue):
     #? Finding the contours
     contours, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     #? Defining the variables for counting
     first = True
     count = 0
+    r = 0
+    b = 0
 
     #? Running the loop in the range of contours to detect the triangles
     for cnt in contours:
@@ -76,12 +78,60 @@ def count_triangles(img):
         if len(approx) == 3:
             count += 1
 
-    return count
+            #? Finding the center point of the shape
+            mom = cv2.moments(cnt)
+
+            if mom['m00'] != 0:
+                x = int(mom['m10']/mom['m00'])
+                y = int(mom['m01']/mom['m00'])
+
+                #? Checking if the center point is red or blue
+                if red[y][x] == 255:
+                    r += 1
+
+                elif blue[y][x] == 255:
+                    b += 1
+
+                else:
+                    count -= 1
 
 
-huts_burnt = count_triangles(mask_brown)
-huts_unburnt = count_triangles(mask_green)
+    return count, r, b
+
+
+#? Defining the range of colors
+lower_red = np.array([0, 0, 0])
+upper_red = np.array([0, 255, 255])
+
+lower_blue = np.array([115, 150, 0])
+upper_blue = np.array([135, 255, 255])
+
+#? Creating the mask and filtering the image to check the color of the triangles
+mask_red = cv2.inRange(hsv, lower_red, upper_red)
+
+mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+
+
+#? calling the functions
+huts_burnt, red_huts_burnt, blue_huts_burnt = count_triangles(mask_brown, mask_red, mask_blue)
+huts_unburnt, red_huts_unburnt, blue_huts_unburnt = count_triangles(mask_green, mask_red, mask_blue)
 
 n_houses = [huts_burnt, huts_unburnt]
 
+
+#? Calculating the priority of the houses on burnt and unburnt grass
+priority_burnt = red_huts_burnt + blue_huts_burnt*2
+
+priority_unburnt = red_huts_unburnt + blue_huts_unburnt*2
+
+priority_houses = [priority_burnt, priority_unburnt]
+
+
+#? Calculating the priority ratio
+priority_ratio = [round(priority_burnt/priority_unburnt, 2)]
+
+
+#? Printing the results
 print('n_houses:', n_houses)
+print('priority_houses:', priority_houses)
+print('priority_ratio:', priority_ratio)
